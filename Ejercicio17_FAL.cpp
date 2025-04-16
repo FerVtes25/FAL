614057f71cd5a1c8477c200990a0876662594731
FerVtes25 <120424378+FerVtes25@users.noreply.github.com>
2025-04-16 13:22:25 +0200

    Add files via upload

---
+++ Ejercicio17_FAL.cpp
@@ -0,0 +1,90 @@
+﻿// Fernando Vieites Moreira
+// FAL-A68
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+// función que resuelve el problema
+/*
+TipoSolucion resolver(TipoDatos datos) {
+
+
+}*/
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+    // leer los datos de la entrada
+    int n;
+    int total = 0;
+    cin >> n;
+
+    if (n == -1)
+        return false;
+
+    vector <int> valores(n);
+    vector <int> v(n);
+    if (n > 0) {
+        cin >> valores[0];
+        total += valores[0];
+    }
+    
+    for (int i = 1; i < n; i++) {
+        cin >> valores[i];
+        total += valores[i];
+        valores[i] += valores[i - 1];
+    }
+
+    v[0] = total;
+    for (int i = 1; i < n; i++) {
+        v[i] = total - valores[i - 1];
+    }
+
+    int P;
+    cin >> P;
+    vector <int> dias(P);
+    for (int i = 0; i < P; i++) {
+        cin >> dias[i];
+    }
+
+    for (int i = 0; i < P; i++) {
+        if (dias[i] == n + 1) {
+            cout << "0" << "\n";
+        }
+        else {
+            cout << v[dias[i] - 1] << "\n";
+        }
+    }
+
+    cout << "---" << "\n";
+
+    return true;
+
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("datos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    while (resuelveCaso())
+        ;
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}
---
+++ Examen2_FAL.cpp
@@ -0,0 +1,85 @@
+﻿// Fernando Vieites Moreira
+// FAL-A68
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+// función que resuelve el problema
+/*
+TipoSolucion resolver(TipoDatos datos) {
+
+
+}*/
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+void resuelveCaso() {
+    // leer los datos de la entrada
+    int a;
+    int numElems;
+    int veces_minimo = 1;
+    int minimo;
+    long long int suma = 0;
+
+    cin >> numElems;
+    
+    //vector <int> numeros;
+
+    cin >> a;
+    minimo = a;
+    //numeros.push_back(a); 
+
+    for (int i = 0; i < numElems - 1; i++) {
+
+        cin >> a;
+
+        if (a == minimo) {
+            veces_minimo++;
+        }
+        else if (a < minimo) {
+            suma += (minimo * veces_minimo);
+            veces_minimo = 1;
+            minimo = a;
+        }
+        else {
+            suma += a;
+        }
+    }
+
+   
+
+    //TipoSolucion sol = resolver(datos);
+    // escribir sol
+
+    cout << suma << " " << numElems - veces_minimo << endl;
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+    
+#ifndef DOMJUDGE
+    std::ifstream in("datos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    int numCasos;
+    std::cin >> numCasos;
+    for (int i = 0; i < numCasos; ++i)
+        resuelveCaso();
+
+    
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+return 0;
+}
